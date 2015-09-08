bc4de3bca39c3ec8d595645d82303569bd1cf285
Nuno Serro <ngserro@gmail.com>
2015-09-08 21:19:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# earthwall
+
+earthwall.sh is a simple bash script to change your OSX wallpaper to a random image from [earthview.withgoogle.com](https://earthview.withgoogle.com/).
+The script was tested on Yosemite and Lion.
+You can set up a periodic wallpaper change using cron. Take a look at the example in [Cron](https://github.com/ngserro/earthwall#cron).
+
+## Installation
+
+The script only supports OSX and requires an internet connection to download the wallpaper.
+Install the script using any of the methods bellow and afterwards make it executable by running: 
+
+```bash
+chmod +x earthwall.sh
+```
+
+### Install using Git
+
+The repository can be cloned to any location.
+
+```bash
+git clone https://github.com/ngserro/earthwall.git
+```
+
+### Git-free installation
+
+To install without Git:
+
+```bash
+cd; curl -#L https://github.com/ngserro/earthwall/tarball/master | tar -xzv 
+```
+
+If you want to update you can simply run that command again.
+
+## Usage
+
+Make sure you are in the directory where the script is located and run:
+
+```bash
+earthwall.sh 
+```
+That's it! Your wallpaper is now an image from earthview.withgoogle.com.
+
+## Cron
+
+If you want your wallpaper to change periodically you can set up the script execution in crontab:
+
+```bash
+crontab -e
+```
+
+Then add a line like this:
+
+```bash
+0 */2 * * * PATH_TO_SCRIPT/earthwall.sh
+```
+This is an example for changing the wallpaper every 2 hours. Change PATH_TO_SCRIPT accordingly.
---
+++ earthwall.sh
@@ -0,0 +1,59 @@
+#!/bin/bash  
+
+# * Name: earthwall.sh
+# * Description: Downloads random image from earthview.withgoogle.com and sets as wallpaper on OSX
+# * Author: Nuno Serro
+# * Date: 09/07/2015 22:24:11 WEST
+# * License: This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# * Copyright (c) 2015, Nuno Serro
+
+# Test if OSX
+if [ "$(uname -s)" != "Darwin" ] ; then
+	echo "This script only works on OSX"
+	exit 1
+fi
+
+mkdir -p $HOME/Pictures/earthwall
+osversion=$(sw_vers -productVersion | sed 's/10.//1')
+
+# Get page index
+/usr/local/bin/wget -q http://earthview.withgoogle.com -O $HOME/Pictures/earthwall/.index.html 2> /dev/null
+if [ $? -ne 0 ]; then
+	echo "Failed to get index from earthview.withgoogle.com"
+	exit 1
+fi
+
+# Set image url, name and location
+image_url=`cat $HOME/Pictures/earthwall/.index.html | grep prettyearth | grep 'https://www.gstatic.com/prettyearth/assets/full/[0-9]\{0,6\}.jpg' -m 1 -o`
+image_name=`echo $image_url | grep '[0-9]\{0,6\}.jpg' -m 1 -o`
+cat $HOME/Pictures/earthwall/.index.html | grep title=\"View | grep -o 'View.*in' | sed 's/View //g' | sed 's/ in//g' > $HOME/Pictures/earthwall/.image_location
+image_location=`cat $HOME/Pictures/earthwall/.image_location | sed 's/, /,/g' | sed 's/ /_/g'`
+
+# Get image
+/usr/local/bin/wget -q $image_url -O $HOME/Pictures/earthwall/$image_location-$image_name 2> /dev/null
+if [ $? -ne 0 ]; then
+	echo "Failed to get image from www.gstatic.com"
+	exit 1
+fi
+
+# Change wallpaper
+if [ $(bc <<< "$osversion<10.0") -eq "1" ] ; then
+	var=$(echo "osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"$HOME/Pictures/earthwall/$image_location-$image_name\"'")
+	eval $var
+else
+	sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$HOME/Pictures/earthwall/$image_location-$image_name'"
+fi
+killall Dock
+
+echo "Wallpaper changed to $image_location-$image_name"
+exit 0
